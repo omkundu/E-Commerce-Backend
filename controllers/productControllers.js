ebc579b86ad13a5a810a83797855348f5162da3e
omkundu <omkundu156@gmail.com>
2023-11-24 11:31:50 +0530

    product_deleted_implimention_done

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -50,3 +50,29 @@ exports.updateProduct = async (req, res, next) => {
     }
   };
   
+
+  //Delete Prodcut
+
+  exports.deleteProduct = async (req, res, next) => {
+    try {
+      const product = await Product.findById(req.params.id);
+  
+      if (!product) {
+        return res.status(404).json({
+          success: false,
+          message: "Product not found",
+        });
+      }
+  
+      await Product.deleteOne({ _id: req.params.id });
+  
+      res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  };
+  
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,13 +1,13 @@
 // productRoute.js
 const express = require("express");
-const { getAllProducts,createProduct, updateProduct } = require("../controllers/productControllers");
+const { getAllProducts,createProduct, updateProduct, deleteProduct } = require("../controllers/productControllers");
 
 const router = express.Router();
 
 // Define a route for getting all products
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct)
+router.route("/product/:id").put(updateProduct).delete(deleteProduct)
 
 
 // Additional routes can be added here if needed
